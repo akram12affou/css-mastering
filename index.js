be20566842447e81eca174562163ef804363b251
Akram Affou <97305449+akram12affou@users.noreply.github.com>
2023-01-22 15:37:27 +0100

    'changes'

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-const hum = document.getElementsByClassName('hum')
-const hum_cont = document.getElementsByClassName('hum_cont')
-let isopen = false;
-hum_cont[0].addEventListener('click', () => {
-if(!isopen){
-    document.styleSheets[0].addRule('.nav_container','transform: translateX(0vh);')
-    document.styleSheets[0].addRule('.hum::before', ' transform: rotate(45deg)');
-    document.styleSheets[0].addRule('.hum::after', ' transform: rotate(-45deg)');
-    document.styleSheets[0].addRule('.hum', ' background-color:rgba(240, 248, 255, 0);');
-    isopen = true
-}else{
-    document.styleSheets[0].addRule('.nav_container','transform: translateX(35vh);') 
-  document.styleSheets[0].addRule('.hum::before', ' transform: translateY(-10px)');
-    document.styleSheets[0].addRule('.hum::after', ' transform: translateY(10px)');
-    document.styleSheets[0].addRule('.hum', 'background-color:white');
-    isopen=false;
-}
+const toggleButton = document.getElementsByClassName('hum_cont')[0]
+
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const hum = document.getElementsByClassName('hum')[0]
+const h1 = document.getElementsByTagName('h1')[0]
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('navbar-linkss')
+  hum.classList.toggle('humopen')
+//   console.log(hum.classList)
 })
